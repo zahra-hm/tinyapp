57d1be162ae48af5a006e66490ab3aff8505daf4
zahra-hm <zahra520@hotmail.com>
2020-11-10 03:49:10 +0000

    implement functino that returns string of random alphanumeric character

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.youtube.com"
@@ -22,6 +25,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const long = urlDatabase[req.params.shortURL];
   const templateVars = { shortURL: req.params.shortURL, longURL: long} ;
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString(6));         // Respond with 'Ok' (we will replace this)
 });
 
 
@@ -49,3 +57,12 @@ app.get("/hello", (req, res) => {
 // app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 // });
+
+function generateRandomString(length) {
+  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let answer = '';
+  for (let i = 0; i < length; i++) {
+    answer += char.charAt((Math.random()*char.length));
+  }
+  return answer;
+}
