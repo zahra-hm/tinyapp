96ea6d831d3aec88d8fa3a80112413eae4d65761
zahra-hm <zahra520@hotmail.com>
2020-11-10 03:01:30 +0000

    add get route to show form

--- express_server.js
+++ express_server.js
@@ -14,11 +14,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const long = urlDatabase[req.params.shortURL];
   const templateVars = { shortURL: req.params.shortURL, longURL: long} ;
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
